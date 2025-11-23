208fe36613e918d2a72c27ebb8e562e8356d3621
Jehuti-ms <dmoseley@gams.edu.bb>
2025-11-23 14:32:08 -0400

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,1 +1,13 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAO37tTin-BEBEXZNBtWbl57-s2UZAQxL8",
+    authDomain: "agrimetrics-a316c.firebaseapp.com",
+    projectId: "agrimetrics-a316c",
+    storageBucket: "agrimetrics-a316c.firebasestorage.app",
+    messagingSenderId: "464595577104",
+    appId: "1:464595577104:web:5e24c30c38c7b7c6e45d95",
+    measurementId: "G-KQW4GMBDVY"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
