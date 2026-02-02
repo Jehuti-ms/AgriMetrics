f9faacb2b60adc3a020a1d3513155d6db36757ef
Jehuti-ms <dmoseley@gams.edu.bb>
2026-02-01 22:35:15 -0400

    Update sales-record.js

--- modules/sales-record.js
+++ modules/sales-record.js
@@ -1008,6 +1008,8 @@ const SalesRecordModule = {
                     width: 100% !important;
                 }
             }
+
+             </style>
             
             <div class="module-container">
                 <!-- Header -->
